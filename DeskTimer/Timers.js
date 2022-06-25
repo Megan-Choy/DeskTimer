c322053f5d1718a91f3640cc328b541b9dda24e0
Megan Choy <43397124+Megan-Choy@users.noreply.github.com>
2022-06-25 09:28:37 +0000

    messing with timers and buttons

--- DeskTimer/Timers.js
+++ DeskTimer/Timers.js
@@ -1,12 +1,22 @@
-function StartTimer(countDown) {
-  // Executes every second
-  setInterval(function() {
-    var now = newDate().getTime();
-  
-    
-  }, 1000)
-  
+var countDownDate = 0;
+var minutes = 0;
+var seconds = 0;
 
+function setTimer(countDown) {    
+  countDownDate = new Date().getTime() + countDown * 1000;
+}
 
-  
+function countDown() {
+  var timeLeft = countDownDate - new Date().getTime();
+
+  minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+}
+
+function getMinutes() {
+  return minutes;
+}
+
+function getSeconds() {
+  return seconds;
 }
