da419a6825fa671b5d1fd77baf7a7733a44a9178
Megan Choy <43397124+Megan-Choy@users.noreply.github.com>
2022-06-26 01:42:11 +0000

    water timer with validation

--- DeskTimer/script.js
+++ DeskTimer/script.js
@@ -7,19 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
   wButton.disabled = true;
   
   wButton.onclick = function() {
-    let newString = wTime.value;
-
-    let indexOfColon = newString.indexOf(':');
-    let mins = newString.substring(0,indexOfColon);
-    let secs = newString.substring(indexOfColon+1);
-    let total = parseInt(mins) * 60 + parseInt(secs);
-
-    console.log("mins: " + mins);
-    console.log("secs: " + secs);
-    console.log(setTimer(total));
-
-    document.querySelector("#wTimer").innerHTML = newString;
-    wTime.value = ' ';
+    if (countDown("wTime", "wButton")) {
 
     let start = setInterval(function() {
       var timeLeft = countDownDate - new Date().getTime();
@@ -41,7 +29,9 @@ document.addEventListener('DOMContentLoaded', function() {
         clearInterval(start);
       }
     }, 1000)
-    
+    }
+
+    wTime.value = ' ';
     return false;
   }
 
@@ -53,5 +43,36 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function setTimer(countDown) {    
   return countDownDate = new Date().getTime() + countDown * 1000;
+}
+
+function validTime(t) {
+  if(t.length != 5) return false;
+  if(t[2] != ':') return false;
+  for(i = 0; i < 5; i++)
+    {
+      if(i == 2) continue;
+      if(t[i] < '0' || t[i] > '9') return false;
+    }
+  return true;
+}
+
+function countDown(textID, buttonID) {
+  if(!validTime(document.getElementById(textID).value))
+  {
+    document.getElementById("note").innerHTML = "Invalid time. Please input     XX:XX, where X is a digit 0-9";
+    return false;
+  }
+  document.getElementById(buttonID).value = "Pause";
+  document.getElementById(textID).disabled = true;
+  let newString = wTime.value;
+  
+  let indexOfColon = newString.indexOf(':');
+  let mins = newString.substring(0,indexOfColon);
+  let secs = newString.substring(indexOfColon+1);
+  let total = parseInt(mins) * 60 + parseInt(secs);
+    
+  setTimer(total)
+
+  return true;
 }
 });
