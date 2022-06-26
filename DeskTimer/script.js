b89a1ac0d73e9d6c38153ef98d7459a8eba9143d
Megan Choy <43397124+Megan-Choy@users.noreply.github.com>
2022-06-26 01:03:07 +0000

    got timer working

--- DeskTimer/script.js
+++ DeskTimer/script.js
@@ -1,58 +1,57 @@
-var wTime = change("rTime");
+document.addEventListener('DOMContentLoaded', function() {
 
-var waterTimer = new Timer("water", wTime);
-if (waterTimer.onGoing) {
-  document.getElementById("wButton").onclick = setInterval(waterTimer.start(),1000);
-} else {
-  document.getElementById("wButton").value = "stop";
-}
+  
+  let wTime = document.querySelector('#wTime');
+  let wButton = document.querySelector('#wButton');
 
+  wButton.disabled = true;
+  
+  wButton.onclick = function() {
+    let newString = wTime.value;
 
-function change(name) {
-  document.querySelector('input').addEventListener('change', function() {
-          var time = document.getElementById(name).value;
-          var mins = time.substring(0,2);
-          var secs = time.substring(3, 5);
-          var total = parseInt(mins) * 60 + parseInt(secs);
+    let indexOfColon = newString.indexOf(':');
+    let mins = newString.substring(0,indexOfColon);
+    let secs = newString.substring(indexOfColon+1);
+    let total = parseInt(mins) * 60 + parseInt(secs);
 
-          console.log("mins: " + mins);
-          console.log("secs: " + secs);
-          console.log(total);
-          return setTimer(total);
-        })
-}
+    console.log("mins: " + mins);
+    console.log("secs: " + secs);
+    console.log(setTimer(total));
 
+    document.querySelector("#wTimer").innerHTML = newString;
+    wTime.value = ' ';
 
-function Timer(name, countDownDate) {
-  this.name = name;
-  this.countDownDate = countDownDate;
-  this.minutes = 0;
-  this.seconds = 0;
-  this.onGoing = false;
-  this.start = function() {
-    var timeLeft = countDownDate - new Date().getTime();
+    let start = setInterval(function() {
+      var timeLeft = countDownDate - new Date().getTime();
+  
+      minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  
+      if (timeLeft > 0) {
+        if (minutes < 10) {
+          minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+          seconds = "0" + seconds;
+        }
+        document.querySelector("#wTimer").innerHTML = minutes + ":" + seconds;
+        
+      } else {
+        document.querySelector("#wTimer").innerHTML = "Time is up!";
+        clearInterval(start);
+      }
+    }, 1000)
+    
+    return false;
+  }
 
-    minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-    if (timeLeft > 0) {
-      document.getElementById("rTime").innerHTML = minutes + ":" + seconds;
-      onGoing = true;
-    } else {
-      document.getElementById("rTime").innerHTML = "Times up!";
-      onGoing = false;
+  wTime.onkeyup = function(){
+    if (wTime.value.length > 0) {
+      wButton.disabled = false;
     }
   }
-  this.getMinutes = function() {
-    return minutes;
-  }
-  this.getSeconds = function() {
-    return seconds;
-  }
-}
-
-
 
 function setTimer(countDown) {    
   return countDownDate = new Date().getTime() + countDown * 1000;
 }
+});
